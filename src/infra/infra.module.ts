6bbbaed426c51e8c7482dd88217f485267662814
wesleyara <wes1gom@gmail.com>
2024-10-12 10:22:23 -0300

    feat: create prisma module

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './env-config/env-config.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, PrismaModule],
 })
 export class InfraModule {}
